7c56510dd151ca3b6e9b6fd664c5b9ac31d3ca35
Svmvyv <Samayanader@gmail.com>
2022-03-11 17:06:35 +0300

    finalizied weather API

--- js/weatherAPI.js
+++ js/weatherAPI.js
@@ -1,33 +1,39 @@
 window.addEventListener("load", () => {
     let lon;
     let lat;
-    let apiKey = "9fb1ed892ffd436caa8f19611008a777";
+    let city = "eddah";
     let cityName = document.querySelector(".city");
-    let temperature = document.querySelector(".temperature");
-    let sunrise = document.querySelector(".sunrise");
-    let sunset = document.querySelector(".sunset");
-    let weatherIcon = document.querySelector(".weather-icon-pic")
+    let apiKey = "9fb1ed892ffd436caa8f19611008a777";
+    let Reigon = document.querySelector(".reigon");
+    let tempValue = document.querySelector(".temperature");
+    let tempDesc = document.querySelector(".desc");
+    let sunRise = document.querySelector(".sunrise");
+    let sunSet = document.querySelector(".sunset");
+    let weatherIcon = document.querySelector(".weatherIcon");
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             lon = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${apiKey}&lang=eng`;
+            const api = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&city=${city}&key=${apiKey}&lang=eng`;
 
             fetch(api)
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
-                    const { timezone, temperature, sunrise, sunset } = data.data[0];
-                    const { icon } = data.data[0].weather
+                    const { timezone, temp, sr, sd, city_name } = data.data[0]
+                    const { description, icon } = data.data[0].weather
 
-                    cityName.textContent = timezone;
-                    temperature.textContent = Math.floor(temp) + "°C";
-                    sunrise.textContent = sunrise;
-                    sunset.textContent = sunset;
-                    weatherIcon.src = `https://api.weatherbit.io/static/img/icons/${icon}.png`;
+
+                    Reigon.textContent = timezone;
+                    tempValue.textContent = Math.floor(temp) + "°C";
+                    tempDesc.textContent = description;
+                    sunRise.textContent = sr;
+                    sunSet.textContent = sd;
+                    cityName.textContent = city_name;
+                    weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
                 })
         })
     }
