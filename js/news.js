b0f8d659197dfa3c678d490f11a8e94f4a610fb1
Svmvyv <Samayanader@gmail.com>
2022-03-13 14:47:33 +0300

    linking navbar links to API

--- js/news.js
+++ js/news.js
@@ -1,15 +1,31 @@
 const apiKey = "fb051857391049a1a92e8aab63ca7126"
 
-let url = `https://newsapi.org/v2/everything?q=coronavirus&from=2022-03-11&sortBy=popularity&apiKey=${apiKey}`
+let url = `https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=fb051857391049a1a92e8aab63ca7126`
 
 
 fetch(url)
     .then(response => response.json())
-    .then(data =>
+    .then(data => {
 
-        console.log(data.articles),
+            console.log(data.articles),
 
-        document.querySelector("headlineNews").innerHTML = data.articles.map
+                document.getElementById("news").innerHTML = data.articles.map(news =>
 
+                    `
+                    <div class="col-md-6">
+                    <div class="card" style="width:40rem;">
+                    <img src="${news.urlToImage}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="${news.url}">${news.title}</a></h5>
+                      <p class="card-text">${news.description}</p>
+                      <p class="card-text"><small class="text-muted">${news.publishedAt} | ${news.author} </small></p>
+                      </div>
+                  </div>
+                  </div>
+                  
+                  `
+
+                ).join('')
+        }
 
     );
